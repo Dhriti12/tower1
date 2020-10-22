a5029b2602bbf96ee1c7ae73cc11aa1c712affea
Dhriti12 <71137659+Dhriti12@users.noreply.github.com>
2020-10-22 22:54:40 +0530

    Add files via upload

---
+++ Box2.js
@@ -0,0 +1,21 @@
+class Box2 extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.visibility=30
+    }
+    display(){
+   
+      if(this.body.speed<4){
+        super.display()
+      }
+      else{
+        World.remove(world,this.body)
+        push ()
+        this.visibility=this.visibility-5
+        tint (255,this.visibility);
+        pop ()
+      }
+    }
+    
+  };
+  
